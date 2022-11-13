36fb9a966b8d0fd1cfcad766f2cd95f34b1c2e49
dawnshi <darwinzerox@gmail.com>
2022-11-13 10:44:35 +0800

    rpc v1 josnrpc

--- RPC/v1/server/server.go
+++ RPC/v1/server/server.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
-	"net"
+	"io"
+	"net/http"
+	"net/rpc"
 	"net/rpc/jsonrpc"
 )
 
@@ -15,30 +16,49 @@ func (p *HelloService) Hello(request string, reply *string) error {
 	return nil
 }
 
+// func main() {
+// 	// 用将给客户端访问的名字和HelloService实例注册 RPC 服务
+// 	// rpc.RegisterName("HelloService", new(HelloService))
+// 	RegisterHelloService(new(HelloService))
+
+// 	// HTTP 服务
+// 	// rpc.HandleHTTP()
+// 	// err := http.ListenAndServe(":1234", nil)
+// 	// if err != nil {
+// 	// 	log.Fatal("Http Listen and Server: ", err)
+// 	// }
+
+// 	// TCP 服务
+// 	listener, err := net.Listen("tcp", ":1234")
+// 	if err != nil {
+// 		log.Fatal("ListenTCP error: ", err)
+// 	}
+// 	for {
+// 		conn, err := listener.Accept()
+// 		if err != nil {
+// 			log.Fatal("Accept error: ", err)
+// 		}
+// 		// rpc.ServeConn(conn)
+// 		// 使用 json RPC
+// 		go jsonrpc.ServeConn(conn)
+// 	}
+// }
+
+// curl -X POST http://localhost:1234/jsonrpc  --data '{"method":"HelloService.Hello","params":["world"],"id":0}'
 func main() {
-	// 用将给客户端访问的名字和HelloService实例注册 RPC 服务
-	// rpc.RegisterName("HelloService", new(HelloService))
 	RegisterHelloService(new(HelloService))
 
-	// HTTP 服务
-	// rpc.HandleHTTP()
-	// err := http.ListenAndServe(":1234", nil)
-	// if err != nil {
-	// 	log.Fatal("Http Listen and Server: ", err)
-	// }
-
-	// TCP 服务
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		log.Fatal("ListenTCP error: ", err)
-	}
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatal("Accept error: ", err)
+	// HTTP Fuwu
+	http.HandleFunc("/jsonrpc", func(w http.ResponseWriter, r *http.Request) {
+		var conn io.ReadWriteCloser = struct {
+			io.Writer
+			io.ReadCloser
+		}{
+			ReadCloser: r.Body,
+			Writer:     w,
 		}
-		// rpc.ServeConn(conn)
-		// 使用 json RPC
-		go jsonrpc.ServeConn(conn)
-	}
+		rpc.ServeRequest(jsonrpc.NewServerCodec(conn))
+	})
+
+	http.ListenAndServe(":1234", nil)
 }
